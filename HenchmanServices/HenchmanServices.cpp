1904d5d1ff82f7eed0fab4f755ef1281d77f6e43
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2024-08-16 11:09:03 +1000

    initial commit to dev

---
+++ HenchmanServices/HenchmanServices.cpp
@@ -0,0 +1,12 @@
+﻿// HenchmanServices.cpp : Defines the entry point for the application.
+//
+
+#include "HenchmanServices.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
